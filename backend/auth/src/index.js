1507d832673c4939bdc765c0eb4195ee6c872e59
prth8564 <preetham10107@gmail.com>
2025-04-26 22:39:53 +0530

    minor change

--- backend/auth/src/index.js
+++ backend/auth/src/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json());
 
 app.post('/auth/signup',signup);
-app.post('auth.login',login);
+app.post('/auth/login',login);
 
 app.listen(3000, () => {
     console.log("app running on port 3000...");
