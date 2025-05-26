146a6927f4f69886a63177c1890626fdc3686a61
prth8564 <preetham10107@gmail.com>
2025-05-26 23:13:10 +0530

    routes added

--- backend/messaging/src/index.js
+++ backend/messaging/src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { startConsumer } from './kafka/consumer.js';
-import { createMessages } from './controllers/messageController.js';
+import { createMessages,getMessages,getConversations } from './controllers/messageController.js';
 const app = express();
 app.use(express.json());
 startConsumer();
-app.post('/messages',createMessages);
+app.post('/conversations/createMessage',createMessages);
+app.get('/messaging/conversations/:user_id',getConversations);
+app.get('/messaging/messages/:conversation_id',getMessages);
 
 app.listen(5000,()=>{
     console.log("Server is running on ports 5000");
